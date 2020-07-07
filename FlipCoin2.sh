4d7850c4c3c9f40dec340840e9d743069902c5e2
Saad <skamil60@gmail.com>
2020-07-08 03:42:52 +0530

    Added : Doublet Combination

--- FlipCoin2.sh
+++ FlipCoin2.sh
@@ -3,6 +3,10 @@
 echo "Welcome to the flip coin combination program."
 
 read -p "Enter the number of times you want to flip : " a
+read -p "Enter the choice of combination :
+1) Singlet
+2) Doublet  " b
+
 
 declare -A dictionary
 
@@ -11,7 +15,7 @@ for (( i=1;i<=$a;i++ ))
 do
 
         combination=""
-        for (( j=1;j<=1;j++ ))
+        for (( j=1;j<=$b;j++ ))
         do
 
                 x=$(($RANDOM%2))
@@ -28,8 +32,3 @@ done
 echo ${!dictionary[@]}
 
 echo ${dictionary[@]}
-for key in ${!dictionary[@]}
-do
-        value=${dictionary[$key]}
-        echo "$key $((( $value *100 ) / $a )) %"
-done | sort -k2 -nr | awk 'NR==1{print $1"    " $2"%"}'
